44693eea7c8a71b302222dccad0c8996f6a77a28
Louis Slater <louismcs@hotmail.co.uk>
2021-06-03 14:39:06 +0100

    Client only markdown editor

--- components/editForm/MarkdownEditor.vue
+++ components/editForm/MarkdownEditor.vue
@@ -2,7 +2,9 @@
 <template>
     <div>
         <h3>{{$attrs.title}}</h3>
-        <v-md-editor v-model="$attrs.text" @change="save" @save="save" left-toolbar="undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image" right-toolbar="preview save"></v-md-editor>
+        <client-only>
+            <v-md-editor v-model="$attrs.text" @change="save" @save="save" left-toolbar="undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image" right-toolbar="preview save"></v-md-editor>
+        </client-only>
         <br />
     </div>
 </template>
